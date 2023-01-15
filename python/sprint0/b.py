84fbb8adb92bccdfd9eb6a52623d49f1ee51182a
Denis Smirnov <desm80@yandex.ru>
2023-01-15 19:19:23 +0300

    спринт 0

--- python/sprint0/b.py
+++ python/sprint0/b.py
@@ -1,14 +1,20 @@
 from typing import List, Tuple
 
+
 def zipper(a: List[int], b: List[int]) -> List[int]:
     # Здесь реализация вашего решения
-    pass
+    return sum(map(list, zip(a, b)), [])
+
 
 def read_input() -> Tuple[List[int], List[int]]:
-    n = int(input())
-    a = list(map(int, input().strip().split()))
-    b = list(map(int, input().strip().split()))
+
+    n = int(input('n>'))
+    a = list(map(int, input('a>').strip().split()))
+    b = list(map(int, input('b>').strip().split()))
+    if len(a) != n or len(b) != n:
+        raise Exception("Длина списков не одинакова или не соответствует n")
     return a, b
+
 
 a, b = read_input()
 print(" ".join(map(str, zipper(a, b))))
--- python/sprint0/e.py
+++ python/sprint0/e.py
@@ -1,8 +1,30 @@
 from typing import List, Tuple, Optional
 
+
+# def two_sum(arr: List[int], target_sum: int) -> Optional[Tuple[int, int]]:
+#     previous = set()
+#     for a in arr:
+#         y = target_sum - a
+#         if y in previous:
+#             return a, y
+#         else:
+#             previous.add(a)
+#     return None
+
 def two_sum(arr: List[int], target_sum: int) -> Optional[Tuple[int, int]]:
-    # Здесь реализация вашего решения
-    pass
+    arr.sort()
+    left = 0
+    right = len(arr)-1
+    while left < right:
+        current_sum = arr[left] + arr[right]
+        if current_sum == target_sum:
+            return arr[left], arr[right]
+        if current_sum < target_sum:
+            left += 1
+        else:
+            right += 1
+    return None
+
 
 def read_input() -> Tuple[List[int], int]:
     n = int(input())
@@ -10,11 +32,13 @@ def read_input() -> Tuple[List[int], int]:
     target_sum = int(input())
     return arr, target_sum
 
+
 def print_result(result: Optional[Tuple[int, int]]) -> None:
     if result is None:
         print(None)
     else:
         print(" ".join(map(str, result)))
+
 
 arr, target_sum = read_input()
 print_result(two_sum(arr, target_sum))
