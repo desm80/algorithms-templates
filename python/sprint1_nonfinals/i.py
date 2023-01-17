2b927981f7dfc3f3e2c703753fdb36d415ac1ab8
desm <desm80@yandex.ru>
2023-01-17 16:43:51 +0300

    решено 9 задач

--- python/sprint1_nonfinals/i.py
+++ python/sprint1_nonfinals/i.py
@@ -1,5 +1,14 @@
 def is_power_of_four(number: int) -> bool:
-    # Здесь реализация вашего решения
-    pass
+    power_of_4 = []
+    power = 0
+    result = 0
+    while result < 10000:
+        result = 4 ** power
+        power_of_4.append(result)
+        power += 1
+    if number in power_of_4:
+        return True
+    return False
+
 
 print(is_power_of_four(int(input())))
