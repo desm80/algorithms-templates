83e01ad2c5278b5765ecdb664607f2d1239d116e
Denis Smirnov <desm80@yandex.ru>
2023-02-13 19:48:47 +0300

    спринт 3 задача B

--- python/sprint3/b.py
+++ python/sprint3/b.py
@@ -1,42 +1,56 @@
-PATTERN ={
-    2: 'abc',
-    3: 'def',
-    4: 'ghi',
-    5: 'jkl',
-    6: 'mno',
-    7: 'pqrs',
-    8: 'tuv',
-    9: 'wxyz',
+PATTERN = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz',
 }
 
 
-# def gen_binary(n, prefix):
-#     if n == 0:
-#        print(prefix)
-#     else:
-#         for letter in PATTERN.get(2):
-#             gen_binary(n - 1, prefix + letter)
-#
-#
-# # string = ['abc', ]
-# for letter in PATTERN.get(9):
-#     gen_binary(1, letter)
-
 def gen_binary(string):
-    n = len(string)-1
-    if n == 0:
-       print(prefix)
-    else:
-        for letter in PATTERN.get(2):
-            gen_binary(n - 1, prefix + letter)
+    if string == '':
+        return ['']
+
+    result = []
+    word = PATTERN[string[-1]]
+    for comb in gen_binary(string[:-1:]):
+        print(comb)
+        for c in word:
+            print(c)
+            result.append(comb + c)
+    return result
+
+string = input()
+print(' '.join(gen_binary(string)))
 
 
 
 
 
-string = list(map(int, input()))
-seqs = []
-for number in string:
-    seqs.append(list(PATTERN[number]))
-
-print(*seqs)
+# def combinations(input_string):
+#     d = {'2': 'abc',
+#          '3': 'def',
+#          '4': 'ghi',
+#          '5': 'jkl',
+#          '6': 'mno',
+#          '7': 'pqrs',
+#          '8': 'tuv',
+#          '9': 'wxyz'}
+#
+#     if input_string == '':
+#         return ['']
+#
+#     data = []
+#     word = d[input_string[-1]]
+#
+#     for combination in combinations(input_string[:-1:]):
+#         for c in word:
+#             data.append(combination + c)
+#
+#     return data
+#
+#
+# print(' '.join(combinations(input())))
